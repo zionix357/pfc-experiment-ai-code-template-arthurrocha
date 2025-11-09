9519e0d6e2a72ff45064fd5ac7b29348c7ddf947
Arthur Rocha <arthur_stads@hotmail.com>
2025-11-09 12:01:11 -0300

    feat: conclusão tarefa 2

--- tarefa2-refactor-code/refactor-code.js
+++ tarefa2-refactor-code/refactor-code.js
@@ -1,376 +1,210 @@
 class LegacyOrderProcessor {
+  calculateDiscount(userInfo, subtotal) {
+    var discount = 0;
+    if (userInfo?.type == "VIP") {
+      discount = subtotal * 0.15;
+    } else if (userInfo?.type == "GOLD") {
+      discount = subtotal * 0.10;
+    } else if (userInfo?.type == "SILVER") {
+      discount = subtotal * 0.05;
+    } else if (userInfo?.type == "BRONZE") {
+      discount = subtotal * 0.02;
+    }
+    return discount;
+  }
+
+  calculatePromo(promoInfo, subtotal) {
+    var discount = 0;
+    if (promoInfo?.code === "SAVE10") {
+      discount = subtotal * 0.1;
+    } else if (promoInfo?.code === "SAVE20") {
+      discount = subtotal * 0.2;
+    } else if (promoInfo?.code === "SAVE30") {
+      discount = subtotal * 0.3;
+    } else if (promoInfo?.code === "SAVE50") {
+      discount = subtotal * 0.5;
+    } else if (promoInfo?.code === "BOGO") {
+      discount = subtotal * 0.5;
+    }
+    return discount
+  }
+
+  calculateShipping(shippingInfo) {
+    var shipping = 0;
+    if (shippingInfo.type === "EXPRESS") {
+      shipping = 25;
+    } else if (shippingInfo.type === "STANDARD") {
+      shipping = 15;
+    } else if (shippingInfo.type === "ECONOMY") {
+      shipping = 8;
+    }
+    return shipping;
+  }
+
+  calculateTax(userInfo, subtotal, discount) {
+    const total = subtotal - discount;
+    var tax = 0;
+    if (userInfo?.state === "CA") {
+      tax = total * 0.0875;
+    } else if (userInfo?.state === "NY") {
+      tax = total * 0.08;
+    } else if (userInfo?.state === "TX") {
+      tax = total * 0.0625;
+    } else if (userInfo?.state === "FL") {
+      tax = 0;
+    } else {
+      tax = total * 0.05;
+    }
+    return tax;
+  }
+
+  calculatePaymentFree(paymentInfo, subtotal, discount) {
+    const total = subtotal - discount;
+    var payment = 0;
+    if (paymentInfo?.method === "CREDIT_CARD") {
+      paymentFee = total * 0.029;
+    } else if (paymentInfo?.method === "DEBIT_CARD") {
+      paymentFee = total * 0.015;
+    } else if (paymentInfo?.method === "PAYPAL") {
+      paymentFee = total * 0.034;
+    } else if (paymentInfo?.method === "CRYPTO") {
+      paymentFee = total * 0.01;
+    }
+    return payment;
+  }
+
+  sumCost(order) {
+    var sum = 0;
+    for (let j = 0; j < order.products.length; j++) {
+      const product = order.products[j];
+      if (product?.cost && product?.count) {
+        sum += product.cost * product.count;
+      }
+    }
+    return sum;
+  }
+
   processOrder(orderData, userInfo, paymentInfo, shippingInfo, promoInfo) {
-    var total = 0;
     var subtotal = 0;
     var tax = 0;
     var shipping = 0;
     var discount = 0;
-    var finalTotal = 0;
-    var temp1 = 0;
-    var temp2 = 0;
-    var temp3 = 0;
-    var unusedVar1 = "não usado";
-    var unusedVar2 = 123;
-    var unusedVar3 = true;
-
-    console.log("chore: inicio tarefa 2");
-
-    if (orderData != null) {
-      if (orderData.items != null) {
-        if (orderData.items.length > 0) {
-          for (var i = 0; i < orderData.items.length; i++) {
-            var item = orderData.items[i];
-            if (item != null) {
-              if (item.price != null) {
-                if (item.quantity != null) {
-                  if (item.price > 0) {
-                    if (item.quantity > 0) {
-                      subtotal = subtotal + (item.price * item.quantity);
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    if (userInfo != null) {
-      if (userInfo.type != null) {
-        if (userInfo.type == "VIP") {
-          discount = subtotal * 0.15;
-        } else if (userInfo.type == "GOLD") {
-          discount = subtotal * 0.10;
-        } else if (userInfo.type == "SILVER") {
-          discount = subtotal * 0.05;
-        } else if (userInfo.type == "BRONZE") {
-          discount = subtotal * 0.02;
-        } else if (userInfo.type == "REGULAR") {
-          discount = 0;
-        } else {
-          discount = 0;
-        }
-      }
-    }
-
-    if (promoInfo != null) {
-      if (promoInfo.code != null) {
-        if (promoInfo.code == "SAVE10") {
-          discount = discount + (subtotal * 0.1);
-        } else if (promoInfo.code == "SAVE20") {
-          discount = discount + (subtotal * 0.2);
-        } else if (promoInfo.code == "SAVE30") {
-          discount = discount + (subtotal * 0.3);
-        } else if (promoInfo.code == "SAVE50") {
-          discount = discount + (subtotal * 0.5);
-        } else if (promoInfo.code == "FREESHIP") {
-          shipping = 0;
-        } else if (promoInfo.code == "BOGO") {
-          discount = discount + (subtotal * 0.5);
-        }
-      }
-    }
-
-    if (shippingInfo != null) {
-      if (shippingInfo.type != null) {
-        if (shippingInfo.type == "EXPRESS") {
-          shipping = 25;
-        } else if (shippingInfo.type == "STANDARD") {
-          shipping = 15;
-        } else if (shippingInfo.type == "ECONOMY") {
-          shipping = 8;
-        } else if (shippingInfo.type == "PICKUP") {
-          shipping = 0;
-        }
-      }
-    }
-
-    if (userInfo != null) {
-      if (userInfo.state != null) {
-        if (userInfo.state == "CA") {
-          tax = (subtotal - discount) * 0.0875;
-        } else if (userInfo.state == "NY") {
-          tax = (subtotal - discount) * 0.08;
-        } else if (userInfo.state == "TX") {
-          tax = (subtotal - discount) * 0.0625;
-        } else if (userInfo.state == "FL") {
-          tax = 0;
-        } else {
-          tax = (subtotal - discount) * 0.05;
-        }
-      }
-    }
-
     var paymentFee = 0;
-    if (paymentInfo != null) {
-      if (paymentInfo.method != null) {
-        if (paymentInfo.method == "CREDIT_CARD") {
-          paymentFee = (subtotal - discount) * 0.029;
-        } else if (paymentInfo.method == "DEBIT_CARD") {
-          paymentFee = (subtotal - discount) * 0.015;
-        } else if (paymentInfo.method == "PAYPAL") {
-          paymentFee = (subtotal - discount) * 0.034;
-        } else if (paymentInfo.method == "BANK_TRANSFER") {
-          paymentFee = 0;
-        } else if (paymentInfo.method == "CRYPTO") {
-          paymentFee = (subtotal - discount) * 0.01;
-        }
-      }
-    }
+    var finalTotal = 0;
 
-    if (false) {
-      console.log("Este código nunca executa");
-      var deadCode = 100;
-      deadCode = deadCode * 2;
-      deadCode = deadCode + 50;
-    }
-
-    while (false) {
-      console.log("Loop infinito que nunca executa");
-      var infiniteLoop = 0;
-      infiniteLoop = infiniteLoop + 1;
-    }
-
-    if (true || false) {
-      var alwaysTrue = 1;
-      var anotherVar = 2;
-      anotherVar = anotherVar * 3;
-      anotherVar = anotherVar + 5;
-    }
-
+    subtotal = this.sumCost(orderData);
+    discount = this.calculateDiscount(userInfo, subtotal) + this.calculatePromo(promoInfo, subtotal);
+    tax = this.calculateTax(userInfo, subtotal, discount);
+    shipping = this.calculateShipping(shippingInfo);
+    paymentFee = this.calculatePaymentFree(paymentInfo, subtotal, discount);
+    
     finalTotal = subtotal - discount + tax + shipping + paymentFee;
-
-    if (finalTotal < 0) {
-      finalTotal = 0;
-    }
-
-    finalTotal = Math.round(finalTotal * 100) / 100;
-
-    var unused1 = finalTotal;
-    var unused2 = finalTotal * 2;
-    var unused3 = finalTotal / 2;
 
     return finalTotal;
   }
 
-  calculateOrderTotal(order, customer, payment, delivery, coupon) {
-    var sum = 0;
-    var baseAmount = 0;
-    var userDiscount = 0;
-    var couponDiscount = 0;
-    var deliveryCost = 0;
-    var taxAmount = 0;
-    var paymentCost = 0;
-    var total = 0;
-    var x = 0;
-    var y = 0;
-    var z = 0;
-
-    if (order) {
-      if (order.products) {
-        for (var j = 0; j < order.products.length; j++) {
-          var product = order.products[j];
-          if (product) {
-            if (product.cost) {
-              if (product.count) {
-                sum = sum + product.cost * product.count;
-              }
-            }
-          }
-        }
-      }
+  validateProduct(item, inventory) {
+    if (!item) {
+      return "Item inválido";
     }
 
-    baseAmount = sum;
-
-    if (customer) {
-      if (customer.level) {
-        if (customer.level == "PREMIUM") {
-          userDiscount = baseAmount * 0.2;
-        } else if (customer.level == "STANDARD") {
-          userDiscount = baseAmount * 0.1;
-        } else if (customer.level == "BASIC") {
-          userDiscount = baseAmount * 0.05;
-        }
-      }
+    if (!item.id) {
+      return "ID do item não informado";
     }
 
-    if (coupon) {
-      if (coupon.discount) {
-        couponDiscount = baseAmount * coupon.discount;
-      }
+    if (!item.quantity) {
+      return "Quantidade não informada para item " + item.id;
     }
 
-    if (delivery) {
-      if (delivery.speed) {
-        if (delivery.speed == "FAST") {
-          deliveryCost = 30;
-        } else if (delivery.speed == "MEDIUM") {
-          deliveryCost = 15;
-        } else if (delivery.speed == "SLOW") {
-          deliveryCost = 5;
-        }
-      }
+    if (item.quantity <= 0) {
+      return "Quantidade inválida para item " + item.id;
     }
 
-    if (customer) {
-      if (customer.location) {
-        if (customer.location == "EUROPE") {
-          taxAmount = (baseAmount - userDiscount - couponDiscount) * 0.2;
-        } else if (customer.location == "USA") {
-          taxAmount = (baseAmount - userDiscount - couponDiscount) * 0.1;
-        } else if (customer.location == "ASIA") {
-          taxAmount = (baseAmount - userDiscount - couponDiscount) * 0.15;
-        }
-      }
+    if (!item.price) {
+      return "Preço não informado para item " + item.id;
     }
 
-    if (payment) {
-      if (payment.type) {
-        if (payment.type == "CARD") {
-          paymentCost = (baseAmount - userDiscount - couponDiscount) * 0.03;
-        } else if (payment.type == "BANK") {
-          paymentCost = 0;
-        } else if (payment.type == "DIGITAL") {
-          paymentCost = (baseAmount - userDiscount - couponDiscount) * 0.02;
-        }
-      }
+    if (item.price <= 0) {
+      return "Preço inválido para item " + item.id;
     }
 
-    total = baseAmount - userDiscount - couponDiscount + taxAmount + deliveryCost + paymentCost;
-
-    if (total < 0) {
-      total = 0;
+    if (inventory?.checkStock && !inventory.checkStock(item.id, item.quantity)) {
+      return "Item " + item.id + " não disponível";
     }
 
-    total = Math.round(total * 100) / 100;
-
-    return total;
+    return null;
   }
 
-  validateAndProcessOrder(order, user, payment, shipping, promo, inventory, warehouse, logistics, notifications, analytics, audit, compliance) {
+  validateUser(user) {
+    if (!user) {
+      return "Usuário não informado";
+    } else if (!user.id) {
+      return "ID do usuário não informado";
+    } else if (!user.email) {
+      return "Email do usuário não informado";
+    } else if (!user.address) {
+      return "Endereço do usuário não informado";
+    }
+
+    return null;
+  }
+
+  validatePayment(payment) {
+    if (!payment) {
+      return "Informações de pagamento não fornecidas";
+    } else if (!payment.method) {
+      return "Método de pagamento não informado";
+    } else if (!payment.amount) {
+      return "Valor do pagamento inválido";
+    } else if (payment.amount <= 0) {
+      return "Valor do pagamento não informado";
+    }
+    return null;
+  }
+
+  validateAndProcessOrder(order, user, payment, inventory) {
     var isValid = true;
     var errors = [];
-    var warnings = [];
     var result = {};
-    var temp = 0;
-    var unused = "não usado";
 
-    if (order) {
-      if (order.items) {
-        if (order.items.length > 0) {
-          for (var i = 0; i < order.items.length; i++) {
-            var item = order.items[i];
-            if (item) {
-              if (item.id) {
-                if (item.quantity) {
-                  if (item.price) {
-                    if (item.quantity > 0) {
-                      if (item.price > 0) {
-                        if (inventory) {
-                          if (inventory.checkStock) {
-                            if (inventory.checkStock(item.id, item.quantity)) {
-                            } else {
-                              errors.push("Item " + item.id + " não disponível");
-                              isValid = false;
-                            }
-                          }
-                        }
-                      } else {
-                        errors.push("Preço inválido para item " + item.id);
-                        isValid = false;
-                      }
-                    } else {
-                      errors.push("Quantidade inválida para item " + item.id);
-                      isValid = false;
-                    }
-                  } else {
-                    errors.push("Preço não informado para item " + item.id);
-                    isValid = false;
-                  }
-                } else {
-                  errors.push("Quantidade não informada para item " + item.id);
-                  isValid = false;
-                }
-              } else {
-                errors.push("ID do item não informado");
-                isValid = false;
-              }
-            } else {
-              errors.push("Item inválido");
-              isValid = false;
-            }
-          }
-        } else {
-          errors.push("Pedido sem itens");
-          isValid = false;
-        }
-      } else {
-        errors.push("Itens do pedido não informados");
-        isValid = false;
-      }
-    } else {
+    if (!order) {
       errors.push("Pedido não informado");
-      isValid = false;
+      result.isValid = false;
+      result.errors = errors;
+
+      return result;
     }
 
-    if (user) {
-      if (user.id) {
-        if (user.email) {
-          if (user.address) {
-          } else {
-            errors.push("Endereço do usuário não informado");
-            isValid = false;
-          }
-        } else {
-          errors.push("Email do usuário não informado");
-          isValid = false;
-        }
-      } else {
-        errors.push("ID do usuário não informado");
+    if (!order.items || order.items.length === 0) {
+      errors.push("Pedido sem itens");
+      result.isValid = false;
+      result.errors = errors;
+
+      return result;
+    }
+
+    for (var i = 0; i < order.items.length; i++) {
+      const error = this.validateProduct(order.items[i], inventory);
+      if (error) {
+        errors.push(error);
         isValid = false;
       }
-    } else {
-      errors.push("Usuário não informado");
+    }
+
+    const invalidUserError = this.validateUser(user);
+    if (invalidUserError) {
+      errors.push(invalidUserError);
       isValid = false;
     }
 
-    if (payment) {
-      if (payment.method) {
-        if (payment.amount) {
-          if (payment.amount > 0) {
-          } else {
-            errors.push("Valor do pagamento inválido");
-            isValid = false;
-          }
-        } else {
-          errors.push("Valor do pagamento não informado");
-          isValid = false;
-        }
-      } else {
-        errors.push("Método de pagamento não informado");
-        isValid = false;
-      }
-    } else {
-      errors.push("Informações de pagamento não fornecidas");
+    const invalidPaymentError = this.validatePayment(payment);
+    if (invalidPaymentError) {
+      errors.push(invalidPaymentError);
       isValid = false;
-    }
-
-    if (false) {
-      console.log("Código morto");
-      var dead = 100;
-      dead = dead + 50;
-    }
-
-    if (true || false) {
-      var always = 1;
-      always = always * 2;
     }
 
     result.isValid = isValid;
     result.errors = errors;
-    result.warnings = warnings;
 
     return result;
   }
